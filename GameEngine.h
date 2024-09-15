5cbff69ab519f3891e5dea4e2c430d8fc2bb46a0
Vaniatka <Vaniatka@HOME-PC>
2024-09-15 13:35:24 +0300

    addet some functions

--- GameEngine.h
+++ GameEngine.h
@@ -1,14 +1,26 @@
 #pragma once
 #include <iostream> 
+#include "Snake.h"
+#include "Apple.h"
+#include "Field.h"
 
 struct GameEngine
 {
+	Snake _snake;
+	Field _field;
+	Apple _apple;
+
 	const int res_x = 80, res_y = 25;
 	std::string player_name;
 	int difficulty;
+	bool game_state;
 
 
 	std::string Set_name();
+	int Set_difficulty();
+	void Game_init();
 	void Game_render(std::string name);
-	void Update_game(int dif)
+	void Update_game(int dif);
+
+
 };
